b1bf0d929153e0afb5fd50ba752e7acca97f358d
vicraica^C <victorvicente527@gmail.com>
2025-09-21 15:59:43 -0300

    atualização e correção

--- victor/src/main/java/victor/git/controllers/MathController.java
+++ victor/src/main/java/victor/git/controllers/MathController.java
@@ -3,42 +3,64 @@ package victor.git.controllers;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import victor.git.exeption.UnsupportedMathOperationException;
+
+    @RestController
+    @RequestMapping("/math")
+    public class MathController {
+
+        //http://localhost:8080/math/sum/3/5
+        @RequestMapping("/sum/{numberOne}/{numberTwo}")
+        public Double sum(
+                @PathVariable("numberOne") Double numberOne,
+                @PathVariable("numberTwo") Double numberTwo) {
+            return numberOne + numberTwo;
+
+        }
+        //http://localhost:8080/math/sub/3/5
+
+        @RequestMapping("/sub/{numberOne}/{numberTwo}")
+        public Double sub(
+                @PathVariable("numberOne") Double numberOne,
+                @PathVariable("numberTwo") Double numberTwo) {
+
+            return numberOne - numberTwo;
+        }
+
+        //http://localhost:8080/math/div/3/5
+        @RequestMapping("/div/{numberOne}/{numberTwo}")
+        public Double div(
+                @PathVariable("numberOne") Double numberOne,
+                @PathVariable("numberTwo") Double numberTwo) {
+
+            return numberOne / numberTwo;
 
 
-@RestController
-@RequestMapping("/math")
-public class MathController {
+        }
+        //http://localhost:8080/mult/div/3/5
 
-    //http://localhost:8080/math/sum/3/5
-    @RequestMapping("/sum/{numberOne}/{numberTwo}")
-    public Double sum(
-            @PathVariable("numberOne") String numberOne,
-            @PathVariable("numberTwo") String numberTwo
-    ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new UnsupportedMathOperationException("please set a numeric value!");
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        @RequestMapping("/mult/{numberOne}/{numberTwo}")
+        public Double mult(
+                @PathVariable("numberOne") Double numberOne,
+                @PathVariable("numberTwo") Double numberTwo) {
+            return numberOne * numberTwo;
+        }
+        //http://localhost:8080/media/div/3/5
+
+        @RequestMapping("/media/{numberOne}/{numberTwo}")
+        public Double media(
+                @PathVariable("numberOne") Double numberOne,
+                @PathVariable("numberTwo") Double numberTwo) {
+            return (numberOne + numberTwo) /2 ;
+        }
+        //http://localhost:8080/raiz/5
+        @RequestMapping("/raiz/{number}")
+        public Double raiz(
+                @PathVariable("number") Double number) {
+
+            if (number < 0) {
+                throw new UnsupportedOperationException("It is not possible to get the square root of a negative number!");
+            }
+
+            return Math.sqrt(number);
+        }
     }
-    //http://localhost:8080/math/sum/3/5
-    //http://localhost:8080/math/sum/3/5
-    //http://localhost:8080/math/sum/3/5
-    //http://localhost:8080/math/sum/3/5
-
-
-    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
-        if (strNumber == null || strNumber.isEmpty())
-            throw new UnsupportedMathOperationException("please set a numeric value !");
-        String number = strNumber.replace(",", ".");
-        return Double.parseDouble(number);
-
-    }
-
-    private Boolean isNumeric(String strNumber) {
-        if (strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",", ".");
-        return number.matches("[+-]?[0-9]*\\.?[0-9]=");
-    }
-
-
-}
